9a9e41b9099f8e21d50f7d0d447480cf291e0cbc
Lucas Emmanuel Bais <lucas.bais@gmail.com>
2018-05-31 17:02:00 -0300

    [feature] discount detail inside onetap view (#1051)

--- sdk/src/main/java/com/mercadopago/onetap/components/PaymentDetailContainer.java
+++ sdk/src/main/java/com/mercadopago/onetap/components/PaymentDetailContainer.java
@@ -3,7 +3,10 @@ package com.mercadopago.onetap.components;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
+import com.mercadopago.R;
 import com.mercadopago.components.CompactComponent;
+import com.mercadopago.components.DetailDirectDiscount;
+import com.mercadopago.customviews.MPTextView;
 import com.mercadopago.model.Item;
 import com.mercadopago.viewmodel.OneTapModel;
 import javax.annotation.Nonnull;
@@ -21,17 +24,26 @@ public class PaymentDetailContainer extends CompactComponent<OneTapModel, Void> 
         return null;
     }
 
-    private void addItemDetails(final ViewGroup parent) {
+    private void addItemDetails(@NonNull final ViewGroup parent) {
         for (Item item : props.getCheckoutPreference().getItems()) {
             parent.addView(new DetailItem(item).render(parent));
         }
     }
 
-    private void addDiscount(final ViewGroup parent) {
-        //TODO when we have discounts
+    private void addDiscount(@NonNull final ViewGroup parent) {
         if (props.hasDiscount()) {
-            //Fruta add
+            final View discountView =
+                new DetailDirectDiscount(new DetailDirectDiscount.Props(props.getDiscount(), props.getCampaign()))
+                    .render(parent);
 
+            parent.addView(addDiscountTitle(parent));
+            parent.addView(discountView);
         }
+    }
+
+    private View addDiscountTitle(final ViewGroup parent) {
+        MPTextView title = (MPTextView) inflate(parent, R.layout.mpsdk_view_modal_title);
+        title.setText(R.string.mpsdk_discount);
+        return title;
     }
 }
--- sdk/src/main/java/com/mercadopago/viewmodel/OneTapModel.java
+++ sdk/src/main/java/com/mercadopago/viewmodel/OneTapModel.java
@@ -3,6 +3,7 @@ package com.mercadopago.viewmodel;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import com.mercadopago.model.Campaign;
 import com.mercadopago.model.Discount;
 import com.mercadopago.model.PaymentMethodSearch;
 import com.mercadopago.preferences.CheckoutPreference;
@@ -12,7 +13,8 @@ import java.io.Serializable;
 public class OneTapModel implements Serializable {
 
     private final CheckoutPreference checkoutPreference;
-    private final Discount discount;
+    @Nullable private final Discount discount;
+    @Nullable private final Campaign campaign;
     private final PaymentMethodSearch paymentMethods;
     private final boolean isEscEnabled;
     private final boolean hasExtraAmount;
@@ -22,12 +24,14 @@ public class OneTapModel implements Serializable {
 
     private OneTapModel(@NonNull final CheckoutPreference checkoutPreference,
         @Nullable final Discount discount,
+        @Nullable final Campaign campaign,
         @NonNull final PaymentMethodSearch paymentMethods,
-        boolean isEscEnabled,
+        final boolean isEscEnabled,
         final boolean hasExtraAmount,
         @Nullable final Integer collectorIcon) {
         this.checkoutPreference = checkoutPreference;
         this.discount = discount;
+        this.campaign = campaign;
         this.paymentMethods = paymentMethods;
         this.isEscEnabled = isEscEnabled;
         this.hasExtraAmount = hasExtraAmount;
@@ -38,16 +42,23 @@ public class OneTapModel implements Serializable {
         final ReviewAndConfirmPreferences reviewAndConfirmPreferences) {
         return new OneTapModel(checkoutStateModel.checkoutPreference,
             checkoutStateModel.discount,
+            checkoutStateModel.campaign,
             checkoutStateModel.paymentMethodSearch,
             checkoutStateModel.flowPreference.isESCEnabled(),
             reviewAndConfirmPreferences.hasExtrasAmount(),
             reviewAndConfirmPreferences.getCollectorIcon());
     }
 
+    @Nullable
+    public Campaign getCampaign() {
+        return campaign;
+    }
+
     public CheckoutPreference getCheckoutPreference() {
         return checkoutPreference;
     }
 
+    @Nullable
     public Discount getDiscount() {
         return discount;
     }
@@ -70,6 +81,6 @@ public class OneTapModel implements Serializable {
     }
 
     public boolean hasDiscount() {
-        return discount != null;
+        return discount != null && campaign != null;
     }
 }

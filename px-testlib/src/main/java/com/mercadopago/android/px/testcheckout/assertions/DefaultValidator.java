1609a830a3ec5df962816f92d22bb5a51470ea8b
valeserber <valserber@gmail.com>
2018-09-07 11:37:51 -0300

    [Feature] Adding one tap examples (#1217)
    * adding one tap page object and some more examples
    
    * changed dynamic id
    
    * removing one tap page objects

--- px-testlib/src/main/java/com/mercadopago/android/px/testcheckout/assertions/DefaultValidator.java
+++ px-testlib/src/main/java/com/mercadopago/android/px/testcheckout/assertions/DefaultValidator.java
@@ -1,10 +1,6 @@
 package com.mercadopago.android.px.testcheckout.assertions;
 
 import android.support.annotation.NonNull;
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.view.View;
-import android.widget.TextView;
-import com.mercadopago.android.px.testcheckout.R;
 import com.mercadopago.android.px.testcheckout.pages.CardPage;
 import com.mercadopago.android.px.testcheckout.pages.CashPage;
 import com.mercadopago.android.px.testcheckout.pages.CongratsPage;
@@ -23,16 +19,9 @@ import com.mercadopago.android.px.testcheckout.pages.PaymentMethodPage;
 import com.mercadopago.android.px.testcheckout.pages.ReviewAndConfirmPage;
 import com.mercadopago.android.px.testcheckout.pages.ReviewPaymentMethodsPage;
 import com.mercadopago.android.px.testcheckout.pages.SecurityCodePage;
-import com.mercadopago.android.px.internal.util.textformatter.TextFormatter;
-import org.hamcrest.Matcher;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class DefaultValidator implements CheckoutValidator {
+
     @Override
     public void validate(@NonNull final IssuerPage issuerPage) {
         //TODO implement default PX Validations
@@ -110,8 +99,7 @@ public class DefaultValidator implements CheckoutValidator {
 
     @Override
     public void validate(@NonNull final DiscountDetailPage discountDetailPage) {
-
-
+        //TODO implement default PX Validations
     }
 
     @Override

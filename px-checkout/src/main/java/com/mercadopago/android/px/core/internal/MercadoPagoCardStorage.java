7976bdaad8c6bd17aeeb3fe8970741331fe69a39
Lucas Emmanuel Bais <lucas.bais@mercadolibre.com>
2019-02-27 09:27:49 -0300

    added old signature to cardstorage

--- px-checkout/src/main/java/com/mercadopago/android/px/core/internal/MercadoPagoCardStorage.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/core/internal/MercadoPagoCardStorage.java
@@ -35,4 +35,25 @@ public final class MercadoPagoCardStorage {
         final int requestCode) {
         GuessingCardActivity.startGuessingCardActivityForStorage(context, accessToken, requestCode);
     }
+
+    /**
+     * Starts card storage flow, walking the user through the card details form, with the purpose of storing the card to
+     * "My Cards" section, allowing the user to use it anytime he/she wants.
+     * <p>
+     * The flow can end with a success, returning {@link Activity#RESULT_OK} to the caller, informing that the card
+     * association succeeded.
+     * <p>
+     * In case the flow experiences an error of any kind, the flow will return a {@link Activity#RESULT_CANCELED}
+     * informing that the card association failed.
+     *
+     * @param callerActivity the activity that calls the flow
+     * @param accessToken logged in user access token
+     * @param requestCode it's the number that identifies the checkout flow request for {@link
+     * Activity#onActivityResult(int, int, Intent)}
+     */
+    @Deprecated
+    public static void startCardStorageFlow(@NonNull final Activity callerActivity, @NonNull final String accessToken,
+        final int requestCode) {
+        GuessingCardActivity.startGuessingCardActivityForStorage(callerActivity, accessToken, requestCode);
+    }
 }

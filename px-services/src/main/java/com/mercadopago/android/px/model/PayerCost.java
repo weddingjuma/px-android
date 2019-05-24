bd03c0d049d92cdc61f104da19bf011832605698
Lucas Emmanuel Bais <lucas.bais@mercadolibre.com>
2019-05-24 10:57:06 -0300

    [feature] new agreement params for payer cost (#1707)
    * new agreement params for payer cost
    
    * new dto for account aggrements

--- px-services/src/main/java/com/mercadopago/android/px/model/PayerCost.java
+++ px-services/src/main/java/com/mercadopago/android/px/model/PayerCost.java
@@ -4,54 +4,42 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
-import com.mercadopago.android.px.internal.util.ParcelableUtil;
+import com.mercadopago.android.px.internal.constants.ProcessingModes;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class PayerCost implements Parcelable, Serializable {
+public final class PayerCost implements Parcelable, Serializable {
 
     public static final int NO_SELECTED = -1;
 
+    @NonNull private Integer installments;
     private static final String CFT = "CFT";
     private static final String TEA = "TEA";
-    private Integer installments;
     private List<String> labels;
     private String recommendedMessage;
     private BigDecimal installmentRate;
     private BigDecimal totalAmount;
-    private BigDecimal minAllowedAmount;
-    private BigDecimal maxAllowedAmount;
     private BigDecimal installmentAmount;
 
+    // params to support hybrid mode.
+    private String processingMode;
+    private List<Agreement> agreements;
+
+    @NonNull
     public Integer getInstallments() {
         return installments;
-    }
-
-    public void setInstallments(Integer installments) {
-        this.installments = installments;
     }
 
     public BigDecimal getInstallmentRate() {
         return installmentRate;
     }
 
-    public void setInstallmentRate(BigDecimal installmentRate) {
-        this.installmentRate = installmentRate;
-    }
-
-    public void setTotalAmount(BigDecimal totalAmount) {
-        this.totalAmount = totalAmount;
-    }
-
     public List<String> getLabels() {
         return labels;
-    }
-
-    public void setLabels(List<String> labels) {
-        this.labels = labels;
     }
 
     public BigDecimal getInstallmentAmount() {
@@ -73,7 +61,7 @@ public class PayerCost implements Parcelable, Serializable {
     public Map<String, String> getRates() {
         final Map<String, String> ratesMap = new HashMap<>();
 
-        if (isValidLabels()) {
+        if (hasValidLabels()) {
             for (final String label : labels) {
                 if (label.contains(CFT) || label.contains(TEA)) {
                     final String[] ratesRaw = label.split("\\|");
@@ -87,131 +75,22 @@ public class PayerCost implements Parcelable, Serializable {
         return ratesMap;
     }
 
-    public Boolean hasRates() {
-        return hasTEA() && hasCFT();
-    }
-
-    public Boolean hasCFT() {
-        return getCFTPercent() != null;
-    }
-
-    public Boolean hasTEA() {
-        return getTEAPercent() != null;
-    }
-
-    private Boolean isValidLabels() {
-        return labels != null && labels.size() > 0;
+    private boolean hasValidLabels() {
+        return labels != null && !labels.isEmpty();
     }
 
     public boolean hasMultipleInstallments() {
         return installments != null && installments > 1;
     }
 
-    @Override
-    public String toString() {
-        return installments.toString();
+    @NonNull
+    public String getProcessingMode() {
+        return processingMode == null ? ProcessingModes.AGGREGATOR : processingMode;
     }
 
-    public static final Creator<PayerCost> CREATOR = new Creator<PayerCost>() {
-        @Override
-        public PayerCost createFromParcel(Parcel in) {
-            return new PayerCost(in);
-        }
-
-        @Override
-        public PayerCost[] newArray(int size) {
-            return new PayerCost[size];
-        }
-    };
-
-    @Deprecated
-    @VisibleForTesting
-    public PayerCost() {
-    }
-
-    protected PayerCost(Parcel in) {
-        installments = ParcelableUtil.getOptionalInteger(in);
-        labels = in.createStringArrayList();
-        recommendedMessage = in.readString();
-
-        if (in.readByte() == 0) {
-            installmentRate = null;
-        } else {
-            installmentRate = new BigDecimal(in.readString());
-        }
-
-        if (in.readByte() == 0) {
-            totalAmount = null;
-        } else {
-            totalAmount = new BigDecimal(in.readString());
-        }
-
-        if (in.readByte() == 0) {
-            minAllowedAmount = null;
-        } else {
-            minAllowedAmount = new BigDecimal(in.readString());
-        }
-
-        if (in.readByte() == 0) {
-            maxAllowedAmount = null;
-        } else {
-            maxAllowedAmount = new BigDecimal(in.readString());
-        }
-
-        if (in.readByte() == 0) {
-            installmentAmount = null;
-        } else {
-            installmentAmount = new BigDecimal(in.readString());
-        }
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(final Parcel dest, final int flags) {
-        ParcelableUtil.writeOptional(dest, installments);
-
-        dest.writeStringList(labels);
-
-        dest.writeString(recommendedMessage);
-
-        if (installmentRate == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeString(installmentRate.toString());
-        }
-
-        if (totalAmount == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeString(totalAmount.toString());
-        }
-
-        if (minAllowedAmount == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeString(minAllowedAmount.toString());
-        }
-
-        if (maxAllowedAmount == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeString(maxAllowedAmount.toString());
-        }
-
-        if (installmentAmount == null) {
-            dest.writeByte((byte) 0);
-        } else {
-            dest.writeByte((byte) 1);
-            dest.writeString(installmentAmount.toString());
-        }
+    @NonNull
+    public List<Agreement> getAgreements() {
+        return agreements == null ? Collections.emptyList() : agreements;
     }
 
     @Override
@@ -242,6 +121,11 @@ public class PayerCost implements Parcelable, Serializable {
         return result;
     }
 
+    @Override
+    public String toString() {
+        return installments.toString();
+    }
+
     public static PayerCost getPayerCost(@NonNull final List<PayerCost> payerCosts,
         final int userSelectedPayerCost,
         final int defaultSelected) {
@@ -250,5 +134,122 @@ public class PayerCost implements Parcelable, Serializable {
         } else {
             return payerCosts.get(userSelectedPayerCost);
         }
+    }
+
+    @Deprecated
+    @VisibleForTesting
+    public PayerCost() {
+    }
+
+    @Deprecated
+    public void setLabels(final List<String> labels) {
+        this.labels = labels;
+    }
+
+    @Deprecated
+    public void setInstallments(@NonNull final Integer installments) {
+        this.installments = installments;
+    }
+
+    @Deprecated
+    public void setInstallmentRate(final BigDecimal installmentRate) {
+        this.installmentRate = installmentRate;
+    }
+
+    @Deprecated
+    public void setTotalAmount(final BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    @Deprecated
+    public Boolean hasRates() {
+        return hasTEA() && hasCFT();
+    }
+
+    @Deprecated
+    public Boolean hasCFT() {
+        return getCFTPercent() != null;
+    }
+
+    @Deprecated
+    public Boolean hasTEA() {
+        return getTEAPercent() != null;
+    }
+
+    public static final Creator<PayerCost> CREATOR = new Creator<PayerCost>() {
+        @Override
+        public PayerCost createFromParcel(final Parcel in) {
+            return new PayerCost(in);
+        }
+
+        @Override
+        public PayerCost[] newArray(final int size) {
+            return new PayerCost[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(final Parcel dest, final int flags) {
+        dest.writeInt(installments);
+
+        dest.writeStringList(labels);
+
+        dest.writeString(recommendedMessage);
+
+        if (installmentRate == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeString(installmentRate.toString());
+        }
+
+        if (totalAmount == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeString(totalAmount.toString());
+        }
+
+        if (installmentAmount == null) {
+            dest.writeByte((byte) 0);
+        } else {
+            dest.writeByte((byte) 1);
+            dest.writeString(installmentAmount.toString());
+        }
+
+        dest.writeString(processingMode);
+        dest.writeTypedList(agreements);
+    }
+
+    protected PayerCost(final Parcel in) {
+        installments = in.readInt();
+        labels = in.createStringArrayList();
+        recommendedMessage = in.readString();
+
+        if (in.readByte() == 0) {
+            installmentRate = null;
+        } else {
+            installmentRate = new BigDecimal(in.readString());
+        }
+
+        if (in.readByte() == 0) {
+            totalAmount = null;
+        } else {
+            totalAmount = new BigDecimal(in.readString());
+        }
+
+        if (in.readByte() == 0) {
+            installmentAmount = null;
+        } else {
+            installmentAmount = new BigDecimal(in.readString());
+        }
+
+        processingMode = in.readString();
+        agreements = in.createTypedArrayList(Agreement.CREATOR);
     }
 }

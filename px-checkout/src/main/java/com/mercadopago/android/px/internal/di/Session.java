21bb951d548313f223e8e3038c2ae3e369753032
Guche Mantuano <agustin.mantuano@mercadolibre.com>
2019-06-13 18:01:44 -0300

    Added deprecated method to Session to avoid breaking changes

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/di/Session.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/di/Session.java
@@ -95,6 +95,11 @@ public final class Session extends ApplicationModule implements AmountComponent 
         super(context);
     }
 
+    @Deprecated
+    public static Session getSession(final Context context) {
+        return getInstance();
+    }
+
     public static Session getInstance() {
         if (instance == null) {
             throw new IllegalStateException(

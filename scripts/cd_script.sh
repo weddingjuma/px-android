15a0a4a22b35ef8d30031c0a429b7861ebc8c32f
Cristian Gaggino <cgaggino@gmail.com>
2019-07-24 13:51:04 -0300

    [ci deploy]

--- scripts/cd_script.sh
+++ scripts/cd_script.sh
@@ -2,7 +2,7 @@
 
 init_emulator () {
     echo no | android create avd --force -n test -t android-22 --abi armeabi-v7a
-    emulator -avd test -no-skin -no-audio -no-window &
+    emulator -avd test -no-audio -no-window &
     android-wait-for-emulator
     adb shell input keyevent 82 &
     adb shell setprop dalvik.vm.dexopt-flags v=n,o=v
@@ -25,11 +25,11 @@ else
 fi
 
 if [[ "$LAST_GIT_COMMIT" == *"[ci deploy]"* && "$BRANCH_TO_TAG" == "release/"* ]]; then
-    init_emulator
+    # init_emulator
     # Load properties
     . gradle.properties
     # Running task and tagging
-    ./gradlew :px-checkout:jacocoTestReport && ./gradlew -Pproduction publishAar -q && git remote add origin-travis https://$GITHUB_AUTH_TOKEN@github.com/mercadopago/px-android.git && git fetch origin-travis && git checkout -b $BRANCH_TO_TAG origin-travis/$BRANCH_TO_TAG && git tag -a $version_to_deploy -m "travis deployed version $version_to_deploy" && git push --tags
+    ./gradlew -Pproduction publishAar -q && git remote add origin-travis https://$GITHUB_AUTH_TOKEN@github.com/mercadopago/px-android.git && git fetch origin-travis && git checkout -b $BRANCH_TO_TAG origin-travis/$BRANCH_TO_TAG && git tag -a $version_to_deploy -m "travis deployed version $version_to_deploy" && git push --tags
 else
     ./gradlew :px-checkout:test && ./gradlew publishAar -q
 fi

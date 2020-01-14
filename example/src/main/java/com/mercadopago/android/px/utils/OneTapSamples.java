cde60722da609ae44ffe642691d73c5d6f645976
matiasromar <matias.romar@mercadolibre.com>
2020-01-14 14:57:00 -0300

    test demo

--- example/src/main/java/com/mercadopago/android/px/utils/OneTapSamples.java
+++ example/src/main/java/com/mercadopago/android/px/utils/OneTapSamples.java
@@ -11,6 +11,8 @@ import com.mercadopago.android.px.core.SplitPaymentProcessor;
 import com.mercadopago.android.px.model.GenericPayment;
 import com.mercadopago.android.px.model.Item;
 import com.mercadopago.android.px.model.Payment;
+import com.mercadopago.android.px.model.PaymentType;
+import com.mercadopago.android.px.model.PaymentTypes;
 import com.mercadopago.android.px.model.Sites;
 import com.mercadopago.android.px.preferences.CheckoutPreference;
 import java.math.BigDecimal;
@@ -55,9 +57,13 @@ public final class OneTapSamples {
 
     public static void addAll(final Collection<Pair<String, MercadoPagoCheckout.Builder>> options) {
         int i = 1;
-        options.add(new Pair<>("Saved cards with default installments", startSavedCardsDefaultInstallments()));
-        options.add(new Pair<>(i++ + " - One tap - Should suggest account money (no cards)",
-            startOneTapWithAccountMoneyNoCards()));
+        options.add(new Pair<>(i++ + " - One tap - MLA - with credit card and off methods",
+            startMLAOneTapWithCreditCardAndOffMethods()));
+        options.add(new Pair<>(i++ + " - One tap - MLM - with credit card and off methods",
+            startMLMOneTapWithCreditCardAndOffMethods()));
+        options.add(new Pair<>(i++ + " - One tap - MLA - without credit card", startMLAWithoutCreditAndDebitCard()));
+        options.add(new Pair<>(i++ + " - One tap - MLA - without off methods", startMLAWithoutOffMethods()));
+
         options.add(new Pair<>(i++ + " - One tap - Should suggest account money (debit and credit cards)",
             startOneTapWithAccountMoneyAndCardsDebitCredit()));
         options.add(new Pair<>(i++ + " - One tap - Should suggest debit card (excluded account money)",
@@ -115,20 +121,205 @@ public final class OneTapSamples {
             .setAdvancedConfiguration(new AdvancedConfiguration.Builder().setExpressPaymentEnable(true).build());
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // It should suggest one tap with account money
-    private static MercadoPagoCheckout.Builder startOneTapWithAccountMoneyNoCards() {
+    private static MercadoPagoCheckout.Builder startMLAOneTapWithCreditCardAndOffMethods() {
 
         final GenericPayment payment = getGenericPaymentApproved();
 
-        final CheckoutPreference preference =
-            getCheckoutPreferenceWithPayerEmail(new ArrayList<>(), 120);
+        final List<Item> items = new ArrayList<>();
+        final Item item = new Item.Builder("Android", 1, new BigDecimal(500))
+            .setDescription("Androide")
+            .setPictureUrl("https://www.androidsis.com/wp-content/uploads/2015/08/marshmallow.png")
+            .setId("1234")
+            .build();
+        items.add(item);
+
+        final Collection<String> excludedPaymentTypes = new ArrayList<>();
+        excludedPaymentTypes.add(PaymentTypes.DEBIT_CARD);
+        //excludedPaymentTypes.add(PaymentTypes.CREDIT_CARD);
+
+        final CheckoutPreference preference = new CheckoutPreference.Builder(Sites.ARGENTINA,
+            PAYER_EMAIL_DUMMY, items)
+            .addExcludedPaymentTypes(excludedPaymentTypes)
+            .build();
+
         final PaymentConfiguration paymentConfiguration =
             PaymentConfigurationUtils.create(new SamplePaymentProcessorNoView(payment));
 
-        return new MercadoPagoCheckout.Builder(ONE_TAP_MERCHANT_PUBLIC_KEY, preference, paymentConfiguration)
-            .setPrivateKey(ONE_TAP_PAYER_1_ACCESS_TOKEN)
+        return new MercadoPagoCheckout.Builder("APP_USR-3668ce4b-329e-4663-b833-496a2d06b7c7", "443971311-5aaf2d72-0e92-4c0d-81ca-5b1176dbcfc0")
+            .setPrivateKey("APP_USR-3320780344791828-011417-7e4540adef34805626529f7d12759ae0-514187106")
             .setAdvancedConfiguration(new AdvancedConfiguration.Builder().setExpressPaymentEnable(true).build());
     }
+
+    //MLA Collector
+    //test_user_40859539@testuser.com
+    //qatest7645
+    //Public key:TEST-53c2696b-85c6-4185-ad57-a969d91e2fe4
+    //Access token:TEST-4129514163640002-080619-bfdc029b9f5220768d774f680c19f44f-443971311
+    //Public key:APP_USR-3668ce4b-329e-4663-b833-496a2d06b7c7
+    //Access token:APP_USR-4129514163640002-080619-2e291fd0b5144b87a8dc2f8e5abefbb8-443971311
+
+
+    //MLA Payer
+    //Access token:TEST-8666946229145884-060616-9baa5719327f2ef704b313e1a585f329__LB_LD__-17623406
+
+    //test_user_61602078@testuser.com
+    //qatest1127
+    //Public key:TEST-cfc672e4-2143-442b-afbc-3079f02de7db
+    //Access token:TEST-3320780344791828-011417-6774f44940b508eb8c4c87e7d8161f1b-514187106
+    //Public key:APP_USR-4419b284-393a-4759-a961-e39525a50541
+    //Access token:APP_USR-3320780344791828-011417-7e4540adef34805626529f7d12759ae0-514187106
+
+
+    private static MercadoPagoCheckout.Builder startMLMOneTapWithCreditCardAndOffMethods() {
+
+        final GenericPayment payment = getGenericPaymentApproved();
+
+        final List<Item> items = new ArrayList<>();
+        final Item item = new Item.Builder("Android", 1, new BigDecimal(500))
+            .setDescription("Androide")
+            .setPictureUrl("https://www.androidsis.com/wp-content/uploads/2015/08/marshmallow.png")
+            .setId("1234")
+            .build();
+        items.add(item);
+
+        final Collection<String> excludedPaymentTypes = new ArrayList<>();
+        excludedPaymentTypes.add(PaymentTypes.DEBIT_CARD);
+        excludedPaymentTypes.add(PaymentTypes.CREDIT_CARD);
+
+        final CheckoutPreference preference = new CheckoutPreference.Builder(Sites.MEXICO,
+            "test_user_16686261@testuser.com", items)
+            .addExcludedPaymentTypes(excludedPaymentTypes)
+            .build();
+
+        final PaymentConfiguration paymentConfiguration =
+            PaymentConfigurationUtils.create(new SamplePaymentProcessorNoView(payment));
+
+        return new MercadoPagoCheckout.Builder("APP_USR-c9156fad-4d37-4d19-aa5e-f3338192c140", "241259769-b78cf0df-5d6b-42cc-bfb5-f1118f353c46",
+            paymentConfiguration)
+            .setPrivateKey("APP_USR-4649111276090223-011417-70fbaf62c11a606806fa6b08eeda7a95-514191195")
+            .setAdvancedConfiguration(new AdvancedConfiguration.Builder().setExpressPaymentEnable(true).build());
+    }
+    //MLM Collector
+    //test_user_6240411@testuser.com
+    //qatest9064
+    //Public key:TEST-629c94f7-7757-4303-891d-a2f21becd98f
+    //Access token:TEST-3058575594414176-011909-b10bf3a28de77b25441fb85d3cc322fa__LA_LB__-241259769
+    //Public key:APP_USR-c9156fad-4d37-4d19-aa5e-f3338192c140
+    //Access token:APP_USR-3058575594414176-011909-1a1a757c87765143b3b5f77efe033ea1__LC_LD__-241259769
+
+    //MLM Payer
+    //test_user_71892562@testuser.com
+    //qatest8852
+    //Public key:TEST-edb908ad-b164-4bf5-ab14-842e9658e6d9
+    //Access token:TEST-4649111276090223-011417-1cbf897207e3b7f33b1f095c0084588e-514191195
+    //Public key:APP_USR-fe841928-f4d4-4f33-92ef-d5c60eaee76d
+    //Access token:APP_USR-4649111276090223-011417-70fbaf62c11a606806fa6b08eeda7a95-514191195
+
+    private static MercadoPagoCheckout.Builder startMLAWithoutCreditAndDebitCard() {
+
+        final GenericPayment payment = getGenericPaymentApproved();
+
+        final List<Item> items = new ArrayList<>();
+        final Item item = new Item.Builder("Android", 1, new BigDecimal(500))
+            .setDescription("Androide")
+            .setPictureUrl("https://www.androidsis.com/wp-content/uploads/2015/08/marshmallow.png")
+            .setId("1234")
+            .build();
+        items.add(item);
+
+        final Collection<String> excludedPaymentTypes = new ArrayList<>();
+        excludedPaymentTypes.add(PaymentTypes.DEBIT_CARD);
+        excludedPaymentTypes.add(PaymentTypes.CREDIT_CARD);
+
+        final CheckoutPreference preference = new CheckoutPreference.Builder(Sites.ARGENTINA,
+            PAYER_EMAIL_DUMMY, items)
+            .addExcludedPaymentTypes(excludedPaymentTypes)
+            .build();
+
+        final PaymentConfiguration paymentConfiguration =
+            PaymentConfigurationUtils.create(new SamplePaymentProcessorNoView(payment));
+
+        return new MercadoPagoCheckout.Builder("APP_USR-648a260d-6fd9-4ad7-9284-90f22262c18d", preference,
+            paymentConfiguration)
+            .setPrivateKey("APP_USR-6519316523937252-070516-964fafa7e2c91a2c740155fcb5474280__LA_LD__-261748045")
+            .setAdvancedConfiguration(new AdvancedConfiguration.Builder().setExpressPaymentEnable(true).build());
+    }
+
+    private static MercadoPagoCheckout.Builder startMLAWithoutOffMethods() {
+
+        final GenericPayment payment = getGenericPaymentApproved();
+
+        final List<Item> items = new ArrayList<>();
+        final Item item = new Item.Builder("Android", 1, new BigDecimal(500))
+            .setDescription("Androide")
+            .setPictureUrl("https://www.androidsis.com/wp-content/uploads/2015/08/marshmallow.png")
+            .setId("1234")
+            .build();
+        items.add(item);
+
+        final Collection<String> excludedPaymentTypes = new ArrayList<>();
+        excludedPaymentTypes.add(PaymentTypes.ATM);
+        excludedPaymentTypes.add(PaymentTypes.BANK_TRANSFER);
+        excludedPaymentTypes.add(PaymentTypes.TICKET);
+        excludedPaymentTypes.add(PaymentTypes.DEBIT_CARD);
+        excludedPaymentTypes.add(PaymentTypes.ACCOUNT_MONEY);
+
+        final CheckoutPreference preference = new CheckoutPreference.Builder(Sites.ARGENTINA,
+            PAYER_EMAIL_DUMMY, items)
+            .addExcludedPaymentTypes(excludedPaymentTypes)
+            .build();
+
+        final PaymentConfiguration paymentConfiguration =
+            PaymentConfigurationUtils.create(new SamplePaymentProcessorNoView(payment));
+
+        return new MercadoPagoCheckout.Builder("APP_USR-648a260d-6fd9-4ad7-9284-90f22262c18d", preference,
+            paymentConfiguration)
+            .setPrivateKey("TEST-8666946229145884-060616-9baa5719327f2ef704b313e1a585f329__LB_LD__-176234066")
+            .setAdvancedConfiguration(new AdvancedConfiguration.Builder().setExpressPaymentEnable(true).build());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // It should suggest one tap with account money
     private static MercadoPagoCheckout.Builder startOneTapWithAccountMoneyAndCardsDebitCredit() {
@@ -348,6 +539,10 @@ public final class OneTapSamples {
             .setId("1234")
             .build();
         items.add(item);
+
+        excludedPaymentTypes.add(PaymentTypes.DEBIT_CARD);
+        excludedPaymentTypes.add(PaymentTypes.CREDIT_CARD);
+
         return new CheckoutPreference.Builder(Sites.ARGENTINA,
             PAYER_EMAIL_DUMMY, items)
             .addExcludedPaymentTypes(excludedPaymentTypes)

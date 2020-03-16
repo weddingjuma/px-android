8f23ff0227414443966792aa4b34ab9000d939f3
Guche Mantuano <agustin.mantuano@mercadolibre.com>
2020-03-15 21:33:17 -0300

    CVV remedy

---
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/features/payment_result/remedies/RemediesViewModel.kt
@@ -0,0 +1,10 @@
+package com.mercadopago.android.px.internal.features.payment_result.remedies
+
+import com.mercadopago.android.px.internal.base.BaseViewModel
+
+internal class RemediesViewModel : BaseViewModel(), Remedies.ViewModel {
+
+    override fun onCvvFilled(cvv: String) {
+
+    }
+}

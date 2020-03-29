842f838cc33ca0aba3eaf598302c818099c1e59d
cgaggino <cristian.gaggino@mercadolibre.com>
2020-03-29 17:36:36 -0300

    * Made linkable text view null safe

--- px-checkout/src/main/java/com/mercadopago/android/px/internal/view/LinkableTextView.java
+++ px-checkout/src/main/java/com/mercadopago/android/px/internal/view/LinkableTextView.java
@@ -1,7 +1,6 @@
 package com.mercadopago.android.px.internal.view;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.Spannable;
@@ -9,10 +8,11 @@ import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
-import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
 import android.view.View;
 import com.mercadopago.android.px.internal.features.TermsAndConditionsActivity;
+import com.mercadopago.android.px.internal.util.TextUtil;
+import com.mercadopago.android.px.internal.util.ViewUtils;
 import com.mercadopago.android.px.model.display_info.LinkablePhrase;
 import com.mercadopago.android.px.model.display_info.LinkableText;
 import java.util.Collections;
@@ -39,35 +39,35 @@ public class LinkableTextView extends android.support.v7.widget.AppCompatTextVie
     }
 
     private void render() {
-        if (!model.getText().isEmpty()) {
+        if (TextUtil.isNotEmpty(model.getText())) {
             final Spannable spannableText = new SpannableStringBuilder(model.getText());
-            if (model.getLinkablePhrases() != null) {
-                for (final LinkablePhrase linkablePhrase : model.getLinkablePhrases()) {
-                    final int start = model.getText().indexOf(linkablePhrase.getPhrase());
-                    final int end = start + linkablePhrase.getPhrase().length();
-                    spannableText.setSpan(
-                        new ClickableSpan() {
-                            @Override
-                            public void onClick(@NonNull final View widget) {
-                                onLinkClicked(linkablePhrase);
-                            }
-                        },
-                        start, end, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-                    spannableText
-                        .setSpan(new ForegroundColorSpan(Color.parseColor(linkablePhrase.getTextColor())), start, end,
-                            0);
-                }
+            for (final LinkablePhrase linkablePhrase : model.getLinkablePhrases()) {
+                addLinkToSpannable(spannableText, linkablePhrase);
             }
-
-            setTextColor(Color.parseColor(model.getTextColor()));
+            ViewUtils.setTextColor(this, model.getTextColor());
             setText(spannableText);
             setMovementMethod(LinkMovementMethod.getInstance());
         }
     }
 
+    private void addLinkToSpannable(@NonNull final Spannable spannable, @NonNull final LinkablePhrase link) {
+        final String phrase = link.getPhrase();
+        final int start = TextUtil.isNotEmpty(phrase) ? model.getText().indexOf(phrase) : -1;
+        if (start >= 0) {
+            final int end = start + phrase.length();
+            spannable.setSpan(new ClickableSpan() {
+                @Override
+                public void onClick(@NonNull final View widget) {
+                    onLinkClicked(link);
+                }
+            }, start, end, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+            ViewUtils.setColorInSpannable(link.getTextColor(), start, end, spannable);
+        }
+    }
+
     /* default */ void onLinkClicked(@NonNull final LinkablePhrase linkablePhrase) {
         String data = "";
-        Map<String, String> links = model.getLinks() != null ? model.getLinks() : Collections.emptyMap();
+        Map<String, String> links = model.getLinks();
         if (!links.isEmpty() && installmentSelected != -1) {
             data = links.get(linkablePhrase.getLinkId(installmentSelected));
         } else if (linkablePhrase.getLink() != null || linkablePhrase.getHtml() != null) {
